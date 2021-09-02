f4cacfb13ccac747a2d2651949897ef149bdc9aa
Kornienko Aleksey <kornienko199004@gmail.com>
2021-09-02 22:45:39 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,15 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int row = 0; row < board.length; row += 1) {
+            if (monoHorizontal(board, row) || monoVertical(board, row)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
--- test/ru/job4j/array/MatrixTest.java
+++ test/ru/job4j/array/MatrixTest.java
@@ -28,4 +28,5 @@ public class MatrixTest {
         };
         Assert.assertArrayEquals(expected, result);
     }
+
 }
