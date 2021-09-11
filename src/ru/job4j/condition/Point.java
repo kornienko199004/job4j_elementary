0260093f2bd3efbd0e78c21719ca25237af44f3e
Kornienko Aleksey <kornienko199004@gmail.com>
2021-09-11 09:51:49 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,13 +1,55 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+
+    private int x;
+    private int y;
+    private int z;
+    private boolean is3D = false;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.is3D = true;
+    }
+
+    public double distance(Point that) {
+        if (this.is3D || that.is3D) {
+            return -1D;
+        }
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        if (!(this.is3D && that.is3D)) {
+            return -1D;
+        }
+
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.x, 2) + Math.pow(that.z - this.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+
+        Point c = new Point(0, 0, 0);
+        Point d = new Point(0, 2, 0);
+        double dist3D = c.distance3d(d);
+        System.out.println(dist3D);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -13,7 +13,9 @@ public class PointTest {
         int y1 = 0;
         int x2 = 2;
         int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
@@ -24,7 +26,9 @@ public class PointTest {
         int y1 = 0;
         int x2 = 0;
         int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
@@ -35,7 +39,9 @@ public class PointTest {
         int y1 = 1;
         int x2 = 0;
         int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
@@ -46,7 +52,38 @@ public class PointTest {
         int y1 = 1;
         int x2 = 2;
         int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when3dAnd2dThanMinus1() {
+        double expected = -1;
+        int x1 = 4;
+        int y1 = 1;
+        int z1 = 1;
+        int x2 = 2;
+        int y2 = 0;
+        Point a = new Point(x1, y1, z1);
+        Point b = new Point(x2, y2);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000And020Than2() {
+        double expected = 2.0;
+        int x1 = 0;
+        int y1 = 0;
+        int z1 = 0;
+        int x2 = 0;
+        int y2 = 2;
+        int z2 = 0;
+        Point a = new Point(x1, y1, z1);
+        Point b = new Point(x2, y2, z2);
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
